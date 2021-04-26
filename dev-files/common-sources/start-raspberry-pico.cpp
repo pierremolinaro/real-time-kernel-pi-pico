c54f111a7545731b84028e6b2cce43281741f316
Pierre Molinaro <pierre@pcmolinaro.name>
2021-04-26 23:45:38 +0200

    Paramétrage fréquence processeur dans étapes 01 et 02

--- dev-files/common-sources/start-raspberry-pico.cpp
+++ dev-files/common-sources/start-raspberry-pico.cpp
@@ -393,7 +393,7 @@ static void clock_init (void) {
                    CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
                    CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS,
                    125 * MHZ,
-                   125 * MHZ);
+                   CPU_MHZ * MHZ);
 //     clock_configure(clk_sys,
 //                     CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
 //                     CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS,
