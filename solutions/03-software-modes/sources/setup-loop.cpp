4904266d3e7fb6b9f155ad818ba5d0e201ebd64b
Pierre Molinaro <pierre@pcmolinaro.name>
2021-04-27 10:57:02 +0200

    Ajout étape 03

--- solutions/03-software-modes/sources/setup-loop.cpp
+++ solutions/03-software-modes/sources/setup-loop.cpp
@@ -6,23 +6,27 @@
 void setup (USER_MODE) {
 //--- Configure Systick
   startSystick (MODE) ;
-//--- Configure PTD7 as digital port (input or output)
-  PORTD_PCR (7) = PORT_PCR_MUX (1) ;
-//--- Configure PTD7 as digital output port (output level is low --> led is off)
-  GPIOD_PDDR |= (1 << 7) ;
+//--- Configure GP25 as output digital port
+  PADS_BANK0_GPIO25 =
+    PADS_BANK0_GPIO25_IE // Input enable
+  | PADS_BANK0_GPIO25_DRIVE (1) // Drive strength
+  | PADS_BANK0_GPIO25_SCHMITT // Enable schmitt trigger
+  ;
+  SIO_GPIO_OE_SET = 1 << 25 ;
+  IO_BANK0_GPIO25_CTRL = 5 ;
 }
 
 //--------------------------------------------------------------------------------------------------
 
 void loop (USER_MODE) {
-//--- Drive PTD7 high --> led is on
-  GPIOD_PSOR = 1 << 7 ;
+//--- Drive GP25 high --> led is on
+  SIO_GPIO_OUT_SET = 1 << 25 ;
 //--- Wait...
   busyWaitDuring (MODE_ 250) ;
-//--- Drive PTD7 low --> led is off
-  GPIOD_PCOR = 1 << 7 ;
+//--- Drive GP25 low --> led is off
+  SIO_GPIO_OUT_CLR = 1 << 25 ;
 //--- Wait...
-  busyWaitDuring (MODE_ 250) ;
+  busyWaitDuring (MODE_ 750) ;
 }
 
 //--------------------------------------------------------------------------------------------------
