7c1db11ce093bff45e62f6767d3280eecc6d2a60
Pierre Molinaro <pierre@pcmolinaro.name>
2021-04-30 19:55:57 +0200

    Étape 13, correction context switch

---
+++ solutions/13-task-termination/sources/xtr-step13.h
@@ -0,0 +1,40 @@
+#pragma once
+
+//--------------------------------------------------------------------------------------------------
+
+#include "software-modes.h"
+
+//--------------------------------------------------------------------------------------------------
+//   Task routine type                                                                                                 *
+//--------------------------------------------------------------------------------------------------
+
+typedef void (* RoutineTaskType) (USER_MODE) ;
+
+//--------------------------------------------------------------------------------------------------
+//   kernel_createTask                                                                                                *
+//--------------------------------------------------------------------------------------------------
+
+void kernel_createTask (INIT_MODE_
+                        uint64_t * inStackBufferAddress,
+                        uint32_t inStackBufferSize,
+                        RoutineTaskType inTaskRoutine) ;
+
+//--------------------------------------------------------------------------------------------------
+
+struct TaskControlBlock ;
+
+TaskControlBlock * descriptorPointerForTaskIndex (const uint8_t inTaskIndex) ;
+
+uint8_t indexForDescriptorTask (const TaskControlBlock * inTaskPtr) ; // should be not nullptr
+
+//--------------------------------------------------------------------------------------------------
+//  TASK SELF TERMINATES
+//--------------------------------------------------------------------------------------------------
+
+//$service task.self.terminates
+
+void taskSelfTerminates (USER_MODE) asm ("task.self.terminates") ;
+
+void service_taskSelfTerminates (KERNEL_MODE) asm ("service.task.self.terminates") ;
+
+//--------------------------------------------------------------------------------------------------
