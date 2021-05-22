5709da791047d6c1430dc8f696d559cae2c7b448
Pierre Molinaro <pierre@pcmolinaro.name>
2021-05-22 15:34:31 +0200

    Étape 22

--- solutions/22-critical-section-using-spin-lock/sources/user-tasks-step22.cpp
+++ solutions/22-critical-section-using-spin-lock/sources/user-tasks-step22.cpp
@@ -2,91 +2,10 @@
 
 //--------------------------------------------------------------------------------------------------
 
-static const uint32_t BUFFER_SIZE = 200 ;
-
-//--------------------------------------------------------------------------------------------------
-
-class Buffer {
-//--- Default constructor
-  public: Buffer (void) :
-  mBuffer (),
-  mCount (0),
-  mReadIndex (0),
-  mMutex (1),
-  mReadSemaphore (0),
-  mWriteSemaphore (BUFFER_SIZE) {
-  }
-
-//--- append
-  public: void append (USER_MODE_ const uint8_t * inObject) {
-    mWriteSemaphore.P (MODE) ;
-    mMutex.P (MODE) ;
-      const uint32_t writeIndex = (mCount + mReadIndex) % BUFFER_SIZE ;
-      mBuffer [writeIndex] = inObject ;
-      mCount += 1 ;
-    mMutex.V (MODE) ;
-    mReadSemaphore.V (MODE) ;
-  }
-
-//--- guarded_append
-  public: bool guarded_append (USER_MODE_ const uint8_t * inObject) {
-    const bool accepted = mWriteSemaphore.guarded_P (MODE) ;
-    if (accepted) {
-      mMutex.P (MODE) ;
-        const uint32_t writeIndex = (mCount + mReadIndex) % BUFFER_SIZE ;
-        mBuffer [writeIndex] = inObject ;
-        mCount += 1 ;
-      mMutex.V (MODE) ;
-      mReadSemaphore.V (MODE) ;
-    }
-    return accepted ;
-  }
-
-//--- remove
-  public: const uint8_t * remove (USER_MODE) {
-    mReadSemaphore.P (MODE) ;
-    mMutex.P (MODE) ;
-      const uint8_t * result = mBuffer [mReadIndex] ;
-      mCount -= 1 ;
-      mReadIndex = (mReadIndex + 1) % BUFFER_SIZE ;
-    mMutex.V (MODE) ;
-    mWriteSemaphore.V (MODE) ;
-    return result ;
-  }
-
-//--- guarded_remove
-  public: bool guarded_remove (USER_MODE_ const uint8_t * & outObject) {
-    const bool accepted = mReadSemaphore.guarded_P (MODE) ;
-    if (accepted) {
-      mMutex.P (MODE) ;
-        outObject = mBuffer [mReadIndex] ;
-        mCount -= 1 ;
-        mReadIndex = (mReadIndex + 1) % BUFFER_SIZE ;
-      mMutex.V (MODE) ;
-      mWriteSemaphore.V (MODE) ;
-    }
-    return accepted ;
-  }
-
-//--- Private properties
-  private: const uint8_t * mBuffer [BUFFER_SIZE] ;
-  private: volatile uint32_t mCount = 0 ;
-  private: volatile uint32_t mReadIndex = 0 ;
-  private: Semaphore mMutex ;
-  private: Semaphore mReadSemaphore ;
-  private: Semaphore mWriteSemaphore ;
-
-//--- No copy
-  private: Buffer (const Buffer &) = delete ;
-  private: Buffer & operator = (const Buffer &) = delete ;
-} ;
-
-//--------------------------------------------------------------------------------------------------
-
 static volatile uint32_t gProducerCount = 0 ;
 static volatile uint32_t gProducerCountPerSecond = 0 ;
-static Buffer gFirstBuffer ;
-static Buffer gSecondBuffer ;
+static BoundedBuffer gFirstBuffer ;
+static BoundedBuffer gSecondBuffer ;
 
 //--------------------------------------------------------------------------------------------------
 
@@ -94,7 +13,7 @@ static void producerTask (USER_MODE) {
   uint32_t allocatedBufferSize = 1 ;
   while (1) {
     if (digitalRead (P4_PUSH_BUTTON)) {
-      const uint8_t * object = new uint8_t [allocatedBufferSize] ;
+      uint8_t * object = new uint8_t [allocatedBufferSize] ;
       gFirstBuffer.append (MODE_ object) ;
       gProducerCount += 1 ;
       gProducerCountPerSecond += 1 ;
@@ -171,9 +90,9 @@ static void displayTask (USER_MODE) {
 
 //--------------------------------------------------------------------------------------------------
 
-static uint64_t gStack0 [128] ;
-static uint64_t gStack1 [128] ;
-static uint64_t gStack2 [128] ;
+static uint64_t gStack0 [64] ;
+static uint64_t gStack1 [64] ;
+static uint64_t gStack2 [64] ;
 static uint64_t gStackIntermediateTask [128] ;
 
 //--------------------------------------------------------------------------------------------------
